f9f278b7ff362876994f1ef59b6b3a9556c77386
valeurfin-app <valeurfin.masteryourfinances@gmail.com>
2026-04-24 17:55:14 +0530

    sw.js

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@
 // CACHE_NAME change forces old caches to be deleted
 // skipWaiting + clients.claim ensures instant activation
 
-var CACHE_NAME = 'valeurfin-v5q';
-var APP_VERSION = 'v4.1.5q';
+var CACHE_NAME = 'valeurfin-v5r';
+var APP_VERSION = 'v4.1.5r';
 
 // Files to cache for offline use
 var CACHE_FILES = [
